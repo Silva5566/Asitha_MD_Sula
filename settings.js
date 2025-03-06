4a51231249027d0b08772081270b598a666da22d
Silva5566 <navingayantha77@gmail.com>
2025-03-06 12:20:54 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=y7xEwbDa#5ANxSJV4D_YYLU6VbHIod90LrVayDadL1D_I5uEiLWg' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=irpRFDIa#SPnfgZFAZDzUHrXWpTyyAWGKFGrKXy016u7qol3CH48' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SUDO: process.env.SUDO === undefined ? '94760663483,94710450435' : process.env.SUDO,
